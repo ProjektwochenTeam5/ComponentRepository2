a5c43b744dc24cb45094b0cb9903beec71092749
ProjektwochenTeam5 <gunter.wiesinger@fhwn.ac.at>
2015-06-16 11:23:51 +0200

    Christophs Test

--- ClientAgent/ClientAgent/Program.cs
+++ ClientAgent/ClientAgent/Program.cs
@@ -10,7 +10,7 @@ namespace ClientAgent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo");
+            Console.WriteLine("Hallo 2");
         }
     }
 }
