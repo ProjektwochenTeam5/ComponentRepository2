a9bb42e5cb74d5e7b2213b07115157147319ee50
ProjektwochenTeam5 <gunter.wiesinger@fhwn.ac.at>
2015-06-17 09:36:13 +0200

    Server

--- ClientAgent/Server/Program.cs
+++ ClientAgent/Server/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// ----------------------------------------------------------------------- 
+// <copyright file="PRogram.cs" company="Gunter Wiesinger"> 
+// Copyright (c) Gunter Wiesinger. All rights reserved. 
+// </copyright> 
+// <summary>This application is just for testing purposes.</summary> 
+// <author>Gunter Wiesinger/Auto generated</author> 
+// -----------------------------------------------------------------------
 namespace Server
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            RecBroadcast b = new RecBroadcast();
+            Task t = new Task(new Action(() => b.Recieve()));
+            t.Start();
         }
     }
 }
---
+++ ClientAgent/Server/RecBroadcast.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class RecBroadcast
+    {
+        public void Recieve()
+        {
+            while (true)
+            {
+                Socket sock = new Socket(AddressFamily.InterNetwork,
+                SocketType.Dgram, ProtocolType.Udp);
+
+                IPEndPoint iep = new IPEndPoint(IPAddress.Any, 1234);
+                sock.Bind(iep);
+
+                EndPoint ep = (EndPoint)iep;
+                Console.WriteLine("Ready to receive...");
+
+                byte[] data = new byte[1024];
+                int recv = sock.ReceiveFrom(data, ref ep);
+                string stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine("received: {0}  from: {1}",
+                                      stringData, ep.ToString());
+
+                data = new byte[1024];
+                recv = sock.ReceiveFrom(data, ref ep);
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine("received: {0}  from: {1}",
+                                      stringData, ep.ToString());
+                sock.Close();
+            }
+        }
+    }
+}
