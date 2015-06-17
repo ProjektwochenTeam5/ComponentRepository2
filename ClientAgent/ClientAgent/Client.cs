b5eaeeae480836de193e53b9ca66e218a7926f01
ProjektwochenTeam5 <gunter.wiesinger@fhwn.ac.at>
2015-06-17 09:48:06 +0200

    Client Thread + ThreadArgs 0.1

---
+++ ClientAgent/ClientAgent/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ClientAgent
+{
+    public class Client
+    {
+        private Thread listenerThread;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Client()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TcpClient Listener
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void SendVisible()
+        {
+            UdpClient cl = new UdpClient(new IPEndPoint(IPAddress.Any, 1234));
+            List<byte> msg = new List<byte>();
+            msg.AddRange(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 });
+
+            cl.Send(msg.ToArray(), msg.Count, new IPEndPoint(IPAddress.Broadcast, 1234));
+        }
+
+
+        private static void ListenerThread(object data)
+        {
+
+        }
+    }
+}
---
+++ ClientAgent/ClientAgent/ThreadArgs.cs
@@ -0,0 +1,37 @@
+﻿
+namespace ClientAgent
+{
+    /// <summary>
+    /// Provides a way to communicate with a thread.
+    /// </summary>
+    public class ThreadArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThreadArgs"/> class.
+        /// </summary>
+        public ThreadArgs()
+        {
+            this.Stopped = false;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the thread was stopped.
+        /// </summary>
+        /// <value>
+        ///     Contains a value indicating whether the thread was stopped.
+        /// </value>
+        public bool Stopped
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Stops the thread.
+        /// </summary>
+        public void Stop()
+        {
+            this.Stopped = true;
+        }
+    }
+}
