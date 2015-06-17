f5d535784cae9fc866a69867a5aec575cab5ceb6
ProjektwochenTeam5 <gunter.wiesinger@fhwn.ac.at>
2015-06-17 13:59:54 +0200

    do job request added

---
+++ ClientAgent/ClientServerCommunication/DoJobRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServerCommunication
+{
+    [Serializable]
+    public class DoJobRequest : Message
+    {
+        public byte[] Job { get; set; }
+
+        public Guid? TargetDisplay { get; set; }
+
+    }
+}
