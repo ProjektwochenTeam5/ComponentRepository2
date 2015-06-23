e4064f4f0c4ac30f78c7b7dde50f329eef88718d
ProjektwochenTeam5 <gunter.wiesinger@fhwn.ac.at>
2015-06-23 16:41:50 +0200

    sdfsd

---
+++ ClientAgent/ClientServerCommunication/ExecuteResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServerCommunication
+{
+    [Serializable()]
+    public class ExecuteResponse : Message
+    {
+        public object[] Result { get; set; }
+
+        public bool Error { get; set; }
+
+        public ExecuteResponse()
+        {
+        }
+
+        public ExecuteResponse(object[] result, bool error)
+        {
+            this.Result = result;
+            this.Error = error;
+        }
+
+        /// <summary>
+        /// Gets the message type of the message.
+        /// </summary>
+        /// <value>
+        ///     Contains the message type of the message.
+        /// </value>
+        public override StatusCode MessageType
+        {
+            get { return StatusCode.ExecuteJob; }
+        }
+    }
+}
