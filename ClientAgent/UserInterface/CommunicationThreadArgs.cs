1a07f07208ef95f114d2bbd80fea150601d80571
ProjektwochenTeam5 <gunter.wiesinger@fhwn.ac.at>
2015-06-25 10:28:35 +0200

    Add CommThreadArgs.

---
+++ ClientAgent/UserInterface/CommunicationThreadArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace UserInterface
+{
+    public class CommunicationThreadArgs
+    {
+        public CommunicationThreadArgs(TcpClient client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException("client");
+            }
+
+            this.Client = client;
+        }
+
+        public TcpClient Client
+        {
+            get;
+            private set;
+        }
+    }
+}
